a23dfecf26f37aa48d8d9a97c4f25eafc319283e
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-05-28 18:04:54 -0300

    [feat] update stock in order routine

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PaymentsAdapter } from 'src/adapters/payments.adapter';
 import { PaymentsRepository } from 'src/repositories/payments.repository';
+import { ProductsRepository } from 'src/repositories/products.repository';
 import { OrdersAdapter } from '../adapters/orders.adapter';
 import { CreateOrderDTO } from '../controllers/dto/create-orders.dto';
 import { OrdersRepository } from '../repositories/orders.repository';
@@ -10,12 +11,15 @@ export class OrdersService {
   constructor(
     private readonly ordersRepository: OrdersRepository,
     private readonly paymentsRepository: PaymentsRepository,
+    private readonly productsRepository: ProductsRepository,
   ) {}
 
   async create(createOrderDto: CreateOrderDTO): Promise<void> {
     try {
       const orderToSave = OrdersAdapter.toDatabase(createOrderDto);
       const newOrder = await this.ordersRepository.createOrder(orderToSave);
+
+      await this.productsRepository.subtractStockInBatch(createOrderDto.items);
 
       const paymentToSave = PaymentsAdapter.fromOrderDTOToDatabase(
         createOrderDto,
